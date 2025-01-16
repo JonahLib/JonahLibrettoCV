8e98fc9aee44ff6170e07c87fe164bfa5404ae00
JonahLib <libretto.jonah@gmail.com>
2025-01-16 15:24:26 +0000

    add form project

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -3,8 +3,8 @@ import { CardProps } from "@/typings/card";
 export const PROJECTS: CardProps[] = [
   {
     photo: "form.svg",
-    gitLink: "https://github.com/",
-    siteLink: "https://github.com/",
+    gitLink: "https://github.com/JonahLib/Forms",
+    siteLink: "https://forms-jy5z9k4b6-jonahlibs-projects.vercel.app/",
     title: "Form",
     description: "This project a usable form displaying different form felids",
   },
